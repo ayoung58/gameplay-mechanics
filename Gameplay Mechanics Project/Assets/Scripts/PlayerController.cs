da22d28d465fd1c26c14e9e82d57e3ace21fcbbc
Theinterna <yalvin58@gmail.com>
2024-07-27 15:08:20 -0700

    Move player based on input and camera rotation

---
+++ Gameplay Mechanics Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
