8ae223ce9106dd6376cf09e84ddbce1930592b3a
Theinterna <yalvin58@gmail.com>
2024-07-28 00:50:23 -0700

    Add multiple waves
    This means a new powerup if all enemies are beaten, and one more enemy is added if all enemies are defeated.

--- Gameplay Mechanics Project/Assets/Scripts/SpawnManager.cs
+++ Gameplay Mechanics Project/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    public int waveNumber = 1;
     public float spawnRange = 9;
+    private int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
+    }
+
+    void SpawnEnemyWave(int numEnemies) {
+        for (int i = 0; i < numEnemies; i++) {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = GameObject.FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0) {
+            if (GameObject.Find("PowerIcon(Clone)") == null) {
+                Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
+            }
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
     }
 
     private Vector3 GenerateSpawnPosition() {
